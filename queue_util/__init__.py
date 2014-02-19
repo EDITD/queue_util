1f75bb524d8e20e20eab7e5735a06b59077495bb
Sujay Mansingh <sujay@editd.com>
2014-02-19 18:00:58 +0000

    Attempt to provide a buffered producer.
    The aim is to use an input iterator that will monitor the queue size.
    If the queue size becomes to big, then the producer will stop putting
    items on the queue until the queue size falls to a certain level.

--- queue_util/__init__.py
+++ queue_util/__init__.py
@@ -1,20 +1,3 @@
 from queue_util.consumer import Consumer
 from queue_util.producer import Producer
-
-import json
-
-import requests
-
-
-def get_num_messages(rabbitmq_host, queue_name, port=15672, vhost="%2F", auth=None):
-    """A (very!) approximate attempt to get the number of messages in a queue.
-    It uses the rabbitmq http API (so make sure that is installed).
-    """
-    if not auth:
-        auth = ("guest", "guest")
-    url = "http://{0}:{1}/api/queues/{2}/{3}".format(rabbitmq_host, port, vhost, queue_name)
-
-    response = requests.get(url, auth=auth)
-    
-    queue_data = json.loads(response.content)
-    return queue_data["messages"]
+from queue_util.producer import get_num_messages
