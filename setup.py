43a1b69ee94d3e07febe0d84a23dc83ee64e5539
Germain Chazot <germain@edited.com>
2020-02-03 13:51:44 +0000

    Change package maintainers to Edited

--- setup.py
+++ setup.py
@@ -34,14 +34,14 @@ if __name__ == "__main__":
     setuptools.setup(
         name="queue_util",
         version=get_version(),
-        author="Sujay Mansingh",
-        author_email="sujay.mansingh@gmail.com",
+        author='EDITED devs',
+        author_email='dev@edited.com',
         packages=setuptools.find_packages(),
         scripts=[],
-        url="https://github.com/sujaymansingh/queue_util",
+        url="https://github.com/EDITD/queue_util",
         license="LICENSE.txt",
         description="A set of utilities for consuming (and producing) from a rabbitmq queue",
-        long_description="View the github page (https://github.com/sujaymansingh/queue_util) for more details.",
+        long_description="View the github page (https://github.com/EDITD/queue_util) for more details.",
         install_requires=REQUIREMENTS,
         extras_require={
             'fast': (
